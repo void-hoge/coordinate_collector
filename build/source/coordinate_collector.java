c5da01f07ffdf3db4d30f51b7d72aaa1a5d81e48
voidhoge <coilgunrailgun@gmail.com>
2020-05-13 14:17:39 +0900

    iroiro

--- build/source/coordinate_collector.java
+++ build/source/coordinate_collector.java
@@ -1,18 +1,18 @@
-import processing.core.*;
-import processing.data.*;
-import processing.event.*;
-import processing.opengl.*;
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
 
-import java.util.HashMap;
-import java.util.ArrayList;
-import java.io.File;
-import java.io.BufferedReader;
-import java.io.PrintWriter;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.io.IOException;
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
 
-public class moji extends PApplet {
+public class coordinate_collector extends PApplet {
 
 int currentx;
 int currenty;
@@ -20,7 +20,7 @@ int currenty;
 public void setup() {
 	currentx = mouseX;
 	currenty = mouseY;
-
+	
 }
 
 public void draw() {
@@ -33,7 +33,7 @@ public void draw() {
 }
   public void settings() { 	size(1000,1000); }
   static public void main(String[] passedArgs) {
-    String[] appletArgs = new String[] { "moji" };
+    String[] appletArgs = new String[] { "coordinate_collector" };
     if (passedArgs != null) {
       PApplet.main(concat(appletArgs, passedArgs));
     } else {
